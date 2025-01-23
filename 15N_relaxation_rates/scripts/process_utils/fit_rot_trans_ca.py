54cf5507ec7c168e52419e66e5df771dfbcc5d21
Gabberpal <dayudin78@gmail.com>
2025-01-23 13:26:46 +0300

    add fit_rot_trans_ca

---
+++ 15N_relaxation_rates/scripts/process_utils/fit_rot_trans_ca.py
@@ -0,0 +1,26 @@
+from MDAnalysis.transformations import fit_rot_trans
+import MDAnalysis as mda
+import numpy as np
+from MDAnalysis.analysis import align
+
+
+class fit_rot_trans_ca(fit_rot_trans):
+
+   def __init__(self, ag, reference, plane=None, weights=None, max_threads=1, parallelizable=True):
+      super().__init__(ag, reference, plane=plane, weights=weights, max_threads=max_threads, parallelizable=parallelizable)
+
+      self.mobile_all_atoms_com = self.mobile.atoms.center(self.weights)
+
+      ag_ca = ag.select_atoms("name CA")
+      ref_ca = reference.select_atoms("name CA")
+      self.ref_com_all = self.ref.center(self.weights)
+
+
+      self.ref, self.mobile = align.get_matching_atoms(ref_ca.atoms, 
+                                                       ag_ca.atoms)
+
+      self.weights = align.get_weights(self.ref.atoms, 
+                                       weights=self.weights)
+
+      self.ref_com = self.ref.center(self.weights)
+      self.ref_coordinates = self.ref.atoms.positions - self.ref_com
