5216afc0568679728b1b5d1b96558eb21d8f5e3f
OOLebedenko <oolebedenko@gmail.com>
2025-01-28 12:16:23 +0300

    add template for calculation acorr function

---
+++ 15N_relaxation_rates/scripts/process_utils/calc.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+def Y2m2(vectors):
+    vectors = vectors / np.linalg.norm(vectors, axis=1)[:, np.newaxis]
+    coef = 1.0 / 4.0 * np.sqrt(15.0 / 2.0 / np.pi)
+
+    return coef * (vectors[:, 0] - 1j * vectors[:, 1]) ** 2
+
+
+def Y2m1(vectors):
+    vectors = vectors / np.linalg.norm(vectors, axis=1)[:, np.newaxis]
+    coef = 1.0 / 2.0 * np.sqrt(15.0 / 2.0 / np.pi)
+
+    return coef * (vectors[:, 0] - 1j * vectors[:, 1]) * vectors[:, 2]
+
+
+def Y20(vectors):
+    vectors = vectors / np.linalg.norm(vectors, axis=1)[:, np.newaxis]
+    coef = 1.0 / 4.0 * np.sqrt(5.0 / np.pi)
+
+    return coef * (2.0 * vectors[:, 2] ** 2 - vectors[:, 0] ** 2 - vectors[:, 1] ** 2)
+
+
+def Y2p1(vectors):
+    vectors = vectors / np.linalg.norm(vectors, axis=1)[:, np.newaxis]
+    coef = -1.0 / 2.0 * np.sqrt(15.0 / 2.0 / np.pi)
+
+    return coef * (vectors[:, 0] + 1j * vectors[:, 1]) * vectors[:, 2]
+
+
+def Y2p2(vectors):
+    vectors = vectors / np.linalg.norm(vectors, axis=1)[:, np.newaxis]
+    coef = 1.0 / 4.0 * np.sqrt(15.0 / 2.0 / np.pi)
+
+    return coef * (vectors[:, 0] + 1j * vectors[:, 1]) ** 2
+
+
+def calc_acorr(x):
+    # return real part of autocorrelation function
+    f = np.fft.fft(np.pad(x, len(x), mode='constant'))
+    result = np.fft.ifft(f * np.conj(f))
+    result = result[:len(x)]
+    result /= np.linspace(len(x), 1, len(x))
+    return np.real(result)
+
+
+def calc_acorr_order_2(vectors, limit=-1):
+    vectors = vectors / np.linalg.norm(vectors, axis=1)[:, np.newaxis]
+    res = [calc_acorr(vectors) for vectors in [Y2m2(vectors), Y2m1(vectors), Y20(vectors)]]
+    res[0] = 2.0 * res[0]
+    res[1] = 2.0 * res[1]
+    return 4.0 * np.pi / 5.0 * np.sum(res, axis=0)
---
+++ 15N_relaxation_rates/scripts/process_utils/save_utils.py
@@ -0,0 +1,27 @@
+from typing import Callable, Iterable
+from process_utils.calc import calc_acorr_order_2
+
+
+def calc_and_save_acorr(path_to_vector_csv_files: Iterable[str],
+                        dt_ns: float,
+                        acorr_func_limit: int = -1,
+                        thumbling_time: float = None,
+                        acorr_func: Callable = calc_acorr_order_2,
+                        out_dir: str = "."):
+    """
+    This function calculates the autocorrelation function for a vector array stored in CSV files and saves the results to an output directory.
+
+    :param path_to_vector_csv_files: Path to a single CSV file or a list of CSV file paths containing the 3-dimensional vector arrays [N x 3].
+    :param dt_ns: Time step (in nanoseconds) between consecutive data points in the vector array
+    :param acorr_func_limit: The maximum lag (in terms of data points) for which the autocorrelation is stored.
+                             If set to -1 (default), the autocorrelation is stored for all possible lags.
+    :param thumbling_time: if is not None the overall tumbling is reintroduced by multiplying the result
+                           by exp(-τ/τ_rot), where τ_rot is rotational correlation time of the protein
+                           Default is None
+    :param acorr_func: The function used to calculate the autocorrelation.
+                       This function should accept a 3D numpy array and return the autocorrelation values.
+                       Default is calc_acorr_order_2.
+    :param out_dir: Directory where the autocorrelation results will be saved. Default is the current directory
+    :return:
+    """
+    pass
