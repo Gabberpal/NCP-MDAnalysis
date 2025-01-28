4c7daf88302680f9190792d58461ab45deb11b61
Gabberpal <dayudin78@gmail.com>
2025-01-28 13:53:10 +0300

    implement fit_and_save_accor_func in save_utils

--- 15N_relaxation_rates/scripts/process_utils/save_utils.py
+++ 15N_relaxation_rates/scripts/process_utils/save_utils.py
@@ -2,10 +2,12 @@ import os
 import pandas as pd
 import numpy as np 
 
+from glob import glob
 from tqdm import tqdm
 
 from typing import Callable, Iterable
 from process_utils.calc import calc_acorr_order_2
+from process_utils.fit import repeated_fit_auto_correlation
 
 
 def calc_and_save_acorr(path_to_vector_csv_files: Iterable[str],
@@ -49,3 +51,58 @@ def calc_and_save_acorr(path_to_vector_csv_files: Iterable[str],
             "time_ns": time_ns,
             "acorr": acorr
         }).to_csv(os.path.join(out_dir, out_name), index=False)
+
+
+def fit_and_save_acorr_func(path_to_acorr_files,
+                            bounds,
+                            p0=None,
+                            lag_spacing="log",
+                            n_lag_points=None,
+                            output_directory="./",
+                            limit_ns=None):
+    path_to_ccr_csv_files = sorted(glob(os.path.join(path_to_acorr_files, "*.csv")))
+    rname_list = []
+    for file in path_to_ccr_csv_files:
+        aa_name = file.split('-')[2]
+        rname_list.append(aa_name)
+    for bound in bounds:
+        tau_table = pd.DataFrame()
+        for acorr_corr_file in tqdm(path_to_ccr_csv_files, desc=output_directory):
+            df = pd.read_csv(acorr_corr_file)
+            if limit_ns:
+                df = df[df["time_ns"] <= limit_ns]
+
+            time_ns, acorr = df["time_ns"].values, df["acorr"].values
+
+            if lag_spacing == "log":
+                lag_index = np.unique(
+                    np.logspace(0, int(np.log10(time_ns.size)), n_lag_points, endpoint=False).astype(int))
+                acorr = np.take(acorr, lag_index)
+                time_ns = np.take(time_ns, lag_index)
+
+            popt = repeated_fit_auto_correlation(acorr, time_ns, bound, p0)
+            name = os.path.splitext(os.path.basename(acorr_corr_file))[0]
+            amplitudes = popt[::2]
+            taus = popt[1::2]
+            order = (len(bound[0]) + 1) // 2
+
+            rid = int(name.split("-")[1])
+
+            popt_dict = {
+                'rId': rid,
+                'rName': rname_list[rid - 2],
+                'limit_ns': limit_ns
+            }
+
+            popt_dict.update(
+                {("exp-%d-a%d" % (order, i + 1)): a for i, a in enumerate(amplitudes)}
+            )
+            popt_dict.update(
+                {("exp-%d-tau%d" % (order, i + 1)): tau for i, tau in enumerate(taus)}
+            )
+
+            tau_table = pd.concat([tau_table, pd.DataFrame(popt_dict, index=[0])])
+
+        tau_table.sort_values(by=['rId'], inplace=True)
+        os.makedirs(output_directory, exist_ok=True)
+        tau_table.to_csv(os.path.join(output_directory, 'tau_%d_exp.csv' % order), index=False)
