911ddbec0136332a6faab8e7535f193ac8808435
Gabberpal <dayudin78@gmail.com>
2025-01-29 16:42:15 +0300

    fix def __init__ in fit_rot_trans_by_pattern

--- 15N_relaxation_rates/scripts/process_utils/fit_rot_trans_by_pattern.py
+++ 15N_relaxation_rates/scripts/process_utils/fit_rot_trans_by_pattern.py
@@ -4,15 +4,13 @@ from MDAnalysis.analysis import align
 
 class fit_rot_trans_by_pattern(fit_rot_trans):
 
-    def __init__(self, ag, reference, pattern=None, *args, **kwargs):
+    def __init__(self, ag, reference, pattern='all', *args, **kwargs):
         super().__init__(ag, reference, *args, **kwargs)
-
-        selection_pattern = pattern if pattern is not None else 'all'
 
         self.mobile_all_atoms_com = self.mobile.atoms.center(self.weights)
 
-        ag_ca = ag.select_atoms(selection_pattern)
-        ref_ca = reference.select_atoms(selection_pattern)
+        ag_ca = ag.select_atoms(pattern)
+        ref_ca = reference.select_atoms(pattern)
         self.ref_com_all = self.ref.center(self.weights)
 
         self.ref, self.mobile = align.get_matching_atoms(ref_ca.atoms,
