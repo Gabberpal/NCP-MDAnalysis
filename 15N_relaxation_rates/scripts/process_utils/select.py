a4837f0cbe6983b6e5b6741ac424e033f111b939
Gabberpal <dayudin78@gmail.com>
2025-01-29 14:51:33 +0300

    add get_sec_str_pattern func in select.py

--- 15N_relaxation_rates/scripts/process_utils/select.py
+++ 15N_relaxation_rates/scripts/process_utils/select.py
@@ -28,3 +28,25 @@ def atom_pair_selecetor(atom_name_1: str,
         ]
 
     return select
+
+
+def get_sec_str_pattern(reference: Universe,
+                        cnain_ids: Iterable[str]
+                        ) -> str:
+    """
+    :param reference: srtucture to assign secondary-structured elements
+    :param chainIDs: list of chain names: ["A"] or ["A", "B"]
+    :return: string pattern for selection of atoms from secondary structured regions in specified chains
+    """
+    sec_str_patterns = []
+    for chain in cnain_ids:
+
+        dssp_results = dssp.DSSP(reference.select_atoms(f"chainID {chain}")).run().results
+        ss_indexes = dssp_results.dssp_ndarray[0][:, 1:].sum(axis=1).astype(bool)
+        ss_residues = dssp_results.resids[ss_indexes]
+
+        if len(ss_residues) > 0:
+            ss_selection = f"(chainID {chain} and resid {' '.join(ss_residues.astype(str))})"
+            sec_str_patterns.append(ss_selection)
+
+    return f"({' or '.join(sec_str_patterns)})" if sec_str_patterns else ""
