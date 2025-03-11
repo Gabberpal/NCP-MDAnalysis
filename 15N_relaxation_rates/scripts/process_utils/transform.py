602f6f16aaeaebbfd48ef6f275218f5c4c88231c
OOLebedenko <oolebedenko@gmail.com>
2025-03-11 17:23:27 +0300

    transfer fit_rot_trans_by_pattern in transform.py

--- 15N_relaxation_rates/scripts/process_utils/transform.py
+++ 15N_relaxation_rates/scripts/process_utils/transform.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from MDAnalysis.analysis import align
+from MDAnalysis.transformations import fit_rot_trans
 from MDAnalysis.transformations.base import TransformationBase
 
 
@@ -108,3 +109,24 @@ class AssembleQuaternaryStructure(TransformationBase):
                 shift = v1 * i + v2 * j + v3 * k
                 ts.positions[mol.ix] += shift
         return ts
+
+
+class fit_rot_trans_by_pattern(fit_rot_trans):
+
+    def __init__(self, ag, reference, pattern='all', *args, **kwargs):
+        super().__init__(ag, reference, *args, **kwargs)
+
+        self.mobile_all_atoms_com = self.mobile.atoms.center(self.weights)
+
+        ag_ca = ag.select_atoms(pattern)
+        ref_ca = reference.select_atoms(pattern)
+        self.ref_com_all = self.ref.center(self.weights)
+
+        self.ref, self.mobile = align.get_matching_atoms(ref_ca.atoms,
+                                                         ag_ca.atoms)
+
+        self.weights = align.get_weights(self.ref.atoms,
+                                         weights=self.weights)
+
+        self.ref_com = self.ref.center(self.weights)
+        self.ref_coordinates = self.ref.atoms.positions - self.ref_com
